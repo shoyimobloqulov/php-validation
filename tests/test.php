e2bdf711255fec1db9e8b249dbde65a3a5ad4a12
shoyimobloqulov <obloqulovshoyim1@gmail.com>
2024-09-16 15:47:25 +0500

    test

---
+++ tests/test.php
@@ -0,0 +1,23 @@
+<?php
+    
+require 'vendor/autoload.php';
+
+use Shoyim\PhpValidation\Validator;
+
+$params = [
+    'username' => 'shoyimobloqulov',
+    'email' => 'admin@gmail.com',
+    'password' => '123'
+];
+
+$validateRules = [
+    'email' => 'required|email',
+    'username' => 'required|string|min:5|max:10',
+    'password' => 'required|password'
+];
+
+$validator = new Validator();
+$validator->validate($validateRules, $params);
+
+var_dump($validator->fails());
+var_dump($validator->getErrors());
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'shoyim/php-validation',
+        'pretty_version' => '0.0.1',
+        'version' => '0.0.1.0',
+        'reference' => null,
+        'type' => 'composer-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'shoyim/php-validation' => array(
+            'pretty_version' => '0.0.1',
+            'version' => '0.0.1.0',
+            'reference' => null,
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
